d1bd4a4012437574fb0fb256c018293f8453904d
Patrick Barabe <16281+paddyredbeard@users.noreply.github.com>
2022-06-05 07:29:46 -0700

    Refactor commanline args evaluation and rebuild

--- src/index.js
+++ src/index.js
@@ -4,7 +4,17 @@ const helper = require('./main.js')
 const solutionsUri = process.env.SOLUTIONS_URL ||
   'https://gist.githubusercontent.com/paddyredbeard/95cec9ed4a7a15b4c7209ba8502438de/raw/e73c6698ada392ec955f8d3b985bb9de995fd6ad/wordle-solutions.txt'
 
-if (typeof args.pattern === 'undefined') {
+let pattern
+
+if (typeof args.pattern !== 'undefined') {
+  pattern = args.pattern
+}
+
+if (typeof args._[0] !== 'undefined') {
+  pattern = args._[0]
+}
+
+if (pattern.length === 0) {
   console.error('No pattern requested')
   process.exit(1)
 }
@@ -15,7 +25,7 @@ if (typeof args.pattern === 'undefined') {
 axios.get(solutionsUri)
   .then(function (response) {
     // handle success
-    const matches = helper.findMatches(args.pattern, response.data)
+    const matches = helper.findMatches(pattern, response.data)
     console.log(matches)
     return matches
   })
